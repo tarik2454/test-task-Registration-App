590d6c079c1c19377174fd69e2b99cb61ec89555
Taras Kovalyk <tarik2454@gmail.com>
2024-05-19 17:40:30 +0300

    adds changes to base config

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/vite-project-template/',
+  base: '/test-task-Registration-App/',
 });
