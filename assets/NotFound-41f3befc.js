2dd861023b137a265672dadd5dbebe309a14a550
tarik2454 <tarik2454@users.noreply.github.com>
2024-05-21 07:18:59 +0000

    deploy: 64d16ab6e246decc9214732715282ef5234792a4

--- assets/NotFound-41f3befc.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./index-a22f714f.js";function n(){return o.jsx("div",{children:"NotFound"})}export{n as default};
-//# sourceMappingURL=NotFound-41f3befc.js.map
---
+++ assets/NotFound-f8b0dfbc.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-6896e714.js";function n(){return o.jsx("div",{children:"NotFound"})}export{n as default};
+//# sourceMappingURL=NotFound-f8b0dfbc.js.map
