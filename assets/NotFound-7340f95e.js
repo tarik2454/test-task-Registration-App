e1234d957865f97fb38cf98b166ad51bee1b308b
tarik2454 <tarik2454@users.noreply.github.com>
2024-05-22 18:50:23 +0000

    deploy: 63b1e4b1fd7dc1bf879fd912a718833b92c653ff

---
+++ assets/NotFound-7340f95e.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-044f175b.js";function n(){return o.jsx("div",{children:"NotFound"})}export{n as default};
+//# sourceMappingURL=NotFound-7340f95e.js.map
--- assets/NotFound-e0b99f74.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./index-8b5b9da1.js";function n(){return o.jsx("div",{children:"NotFound"})}export{n as default};
-//# sourceMappingURL=NotFound-e0b99f74.js.map
