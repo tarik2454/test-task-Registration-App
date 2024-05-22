09ef65ca681d41890f609a088d0a47265f5cd23e
tarik2454 <tarik2454@users.noreply.github.com>
2024-05-22 13:34:20 +0000

    deploy: 8ba0c2bd6c240f0f32faa287996a7c61adc314cf

--- assets/NotFound-70c55181.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./index-72ff9f75.js";function n(){return o.jsx("div",{children:"NotFound"})}export{n as default};
-//# sourceMappingURL=NotFound-70c55181.js.map
---
+++ assets/NotFound-e0b99f74.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-8b5b9da1.js";function n(){return o.jsx("div",{children:"NotFound"})}export{n as default};
+//# sourceMappingURL=NotFound-e0b99f74.js.map
