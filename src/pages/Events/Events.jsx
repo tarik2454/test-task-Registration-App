8bc7f32db5a2c3f6861899df20adea15c7f756fe
Taras Kovalyk <tarik2454@gmail.com>
2024-05-27 20:46:18 +0300

    fix bug loading start page

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 import { getEvents } from '../../apiServices/apiServices';
 
@@ -6,12 +6,9 @@ import Section from '../../components/Section/Section';
 import Container from '../../components/Container/Container';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import Pagination from '../../components/Pagination/Pagination';
-import Loader from '../../components/Loader/Loader';
 
 export default function Events() {
   const [events, setEvents] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const isFirstLoad = useRef(true);
 
   useEffect(() => {
     const getData = async () => {
@@ -20,28 +17,17 @@ export default function Events() {
         setEvents(data);
       } catch (error) {
         console.warn(error.message);
-      } finally {
-        setIsLoading(false);
-        isFirstLoad.current = false;
       }
     };
 
-    if (isFirstLoad.current) {
-      getData();
-    } else {
-      setIsLoading(false);
-    }
+    getData();
   }, []);
 
   return (
     <Section>
       <Container>
         <SectionTitle title={'Events'} />
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <Pagination items={events} itemsPerPage={12} />
-        )}
+        <Pagination items={events} itemsPerPage={12} />
       </Container>
     </Section>
   );
